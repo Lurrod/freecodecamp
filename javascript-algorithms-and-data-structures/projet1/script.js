d79d2b273b831f8d9ee4e7870d34c0e1ce6891ff
lurrod <borde.titouan.47@gmail.com>
2025-01-02 15:06:00 +0100

    new project

--- javascript-algorithms-and-data-structures/projet1/script.js
+++ javascript-algorithms-and-data-structures/projet1/script.js
@@ -1,21 +1,32 @@
+const palindromes = [];
+
 document.getElementById("check-btn").addEventListener("click", () => {
-    const input = document.getElementById("text-input").value;
-    const result = document.getElementById("result");
-  
-    if (!input) {
-      alert("Please input a value");
-      return;
+  const input = document.getElementById("text-input").value;
+  const result = document.getElementById("result");
+  const list = document.getElementById("list");
+
+  if (!input) {
+    alert("Please input a value");
+    return;
+  }
+
+  const sanitizedInput = input
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "");
+  const reversedInput = sanitizedInput.split("").reverse().join("");
+
+  if (sanitizedInput === reversedInput) {
+    result.textContent = `"${input}" is a palindrome!`;
+    result.style.color = "green";
+
+    if (!palindromes.includes(input)) {
+      palindromes.push(input);
+      const li = document.createElement("li");
+      li.textContent = input;
+      list.appendChild(li);
     }
-  
-    const sanitizedInput = input
-      .toLowerCase()
-      .replace(/[^a-z0-9]/g, "");
-    const reversedInput = sanitizedInput.split("").reverse().join("");
-  
-    if (sanitizedInput === reversedInput) {
-      result.textContent = `${input} is a palindrome`;
-    } else {
-      result.textContent = `${input} is not a palindrome`;
-    }
-  });
-  
+  } else {
+    result.textContent = `"${input}" is not a palindrome.`;
+    result.style.color = "red";
+  }
+});
